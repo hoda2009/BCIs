13b3c7901ef073b275594064cf3ea8d68716e409
Hoda <hodanshawy2009@gmail.com>
2026-01-05 22:59:20 +0200

    try

--- index.js
+++ index.js
@@ -40,3 +40,28 @@ for(let i=0;i< hands.length;i++){
 
     }
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+ const reveals = document.querySelectorAll(".reveal");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      } else {
+        entry.target.classList.remove("active");
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  reveals.forEach(el => observer.observe(el));
